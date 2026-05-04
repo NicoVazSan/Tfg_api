8f8ba552a1f11c2034ae574022562991036fcf60
NicoVazSan <nvazquezs24@fpcoruna.afundacion.org>
2026-05-05 00:13:27 +0200

    Creacion endpoints usuarios

--- Tfg_api/Tfg_api/urls.py
+++ Tfg_api/Tfg_api/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core import  endpoints
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/login', endpoints.login_user),
+    path('auth/register', endpoints.register_user),
 ]
